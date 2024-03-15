084e469ae99cbf8fff84cf2e5c43f206a572e6be
carlosjtandem <soycarlosjavier14@gmail.com>
2024-03-15 09:01:44 -0500

    First commit

---
+++ Orders/Orders.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit.Sdk;
+
+namespace Orders.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
