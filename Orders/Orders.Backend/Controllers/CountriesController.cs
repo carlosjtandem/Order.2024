e68d084a8e20586f1895d4c53c5fa17c2a13284a
carlosjtandem <soycarlosjavier14@gmail.com>
2024-03-18 18:43:36 -0500

    CRUD in Country entity

---
+++ Orders/Orders.Backend/Controllers/CountriesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Orders.Backend.Controllers.Data;
+using Orders.Shared.Entities;
+
+namespace Orders.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] //es lo mismo de tener /api/countries
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(Country country)  //IActionResult es la respuesta de una petición http
+        {
+            _context.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(); //retorna respuesta 200
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsync(Country country)
+        {
+            _context.Update(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
