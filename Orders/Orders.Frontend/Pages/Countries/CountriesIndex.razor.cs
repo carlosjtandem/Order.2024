c9206fc93deaf24de6ad59fcea12458606cb24ec
carlosjtandem <soycarlosjavier14@gmail.com>
2024-03-19 16:06:52 -0500

    Creacion de frontend generico para soportar cualquier entidad

---
+++ Orders/Orders.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Repositories;
+using Orders.Shared.Entities;
+
+namespace Orders.Frontend.Pages.Countries
+{
+    public partial class CountriesIndex //Defino partial para poder trabajar por partes
+    {
+        [Inject] private IRepository repository { get; set; } = null;  //Estamos creando un repositorio
+
+        public List<Country>? Countries { get; set; } //? es nulleable porque puede pasar que este vacio
+
+        //parte del ciclo de vida
+        protected async override Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            var responseHttp = await repository.GetAsync<List<Country>>("api/countries");
+
+            Countries = responseHttp.Response;  // aqui devuelve la lista de paises
+        }
+
+
+    }
+}
---
+++ Orders/Orders.Frontend/Shared/GenericList.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Orders.Frontend.Shared
+{
+    //Va a permitir listar cualquier objecto
+    public partial class GenericList<Titem>
+    {
+        [Parameter] public RenderFragment? Loading { get; set; } //Puede ser null
+        [Parameter] public RenderFragment? NoRecords { get; set; } //campo cuando no hay registros
+        [EditorRequired, Parameter] public RenderFragment? Body { get; set; } = null!; // Primer param obligatorio No Puede ser null  
+        [EditorRequired, Parameter] public List<Titem>  MyList { get; set; } = null!; // Segundo param obligatorio .. Es el listado
+    }
+}
